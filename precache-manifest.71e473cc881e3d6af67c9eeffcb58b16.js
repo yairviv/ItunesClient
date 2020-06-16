567205544e86623911a2ca41826b61fc2a8e8afe
Yair Revivo <yrevivo@attentigroup.com>
2020-06-16 20:43:25 +0300

    Updates

---
+++ precache-manifest.71e473cc881e3d6af67c9eeffcb58b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47b9d3097138629ea1f68769509ac6d",
+    "url": "/ItunesClient/index.html"
+  },
+  {
+    "revision": "a44f299c59c25d505873",
+    "url": "/ItunesClient/static/css/main.81131266.chunk.css"
+  },
+  {
+    "revision": "b86a6f3d63bcd4e37aa6",
+    "url": "/ItunesClient/static/js/2.8855a6c1.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/ItunesClient/static/js/2.8855a6c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44f299c59c25d505873",
+    "url": "/ItunesClient/static/js/main.dd02137c.chunk.js"
+  },
+  {
+    "revision": "5dd70e17add6865a8d80",
+    "url": "/ItunesClient/static/js/runtime-main.2f69e83f.js"
+  },
+  {
+    "revision": "2f79694de210f873ca01300355f8631f",
+    "url": "/ItunesClient/static/media/shopping-cart.2f79694d.svg"
+  }
+]);
